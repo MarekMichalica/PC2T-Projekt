90054a636a34fe0edd46681b17d90f68272c4c31
MarekMichalica <mmichalica7@gmail.com>
2024-04-17 13:28:01 +0200

    Vytvorenie nekonecneho cyklu aplikacie, vytvorenie kniznice Hashmap, vytvorenie triedy Kniha a enum pre zanre

---
+++ projekt/src/projekt/Kniznica.java
@@ -0,0 +1,19 @@
+package projekt;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Kniznica {
+	private Map<String, Kniha> knihy;
+	
+	public Kniznica() {
+		this.knihy = new HashMap<>();
+	}
+	
+	public void pridajKnihu(Scanner scanner) {
+		
+	}
+	
+}
--- projekt/src/projekt/Main.java
+++ projekt/src/projekt/Main.java
@@ -1,10 +1,60 @@
 package projekt;
 
+import java.util.Scanner;
+
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		Kniznica kniznica = new Kniznica();
+		
+		while (true) {
+            System.out.println("Vyberte akciu:");
+            System.out.println("1. Pridanie novej knihy");
+            System.out.println("2. Uprava knihy");
+            System.out.println("3. Mazanie knihy");
+            System.out.println("4. Oznacenie knihy ako vypozicana/vratiaca");
+            System.out.println("5. Vypis knih");
+            System.out.println("6. Vyhladanie knihy");
+            System.out.println("7. Vypis vsetkych knih daneho autora");
+            System.out.println("8. Vypis vsetkych knih podla zanru");
+            System.out.println("9. Vypis vsetkych vypozicanych knih s informaciou o type");
+            System.out.println("10. Ulozenie informacie o vybranej knihe do suboru");
+            System.out.println("11. Nacitanie informacie o danej knihe zo suboru");
+            System.out.println("12. Ulozenie vsetkych informacii do databazy a ukoncenie programu");
+            System.out.print("Vasa volba: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
 
+            switch (choice) {
+                case 1:
+                	kniznica.pridajKnihu(scanner);
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    break;
+                case 12:
+                    break;
+                default:
+                    System.out.println("Neplatna volba, zadajte cislo od 1 do 12.");
+            }
+        }
+		
 	}
-
 }
